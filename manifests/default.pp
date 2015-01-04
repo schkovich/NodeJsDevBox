79dd224a05ab60092ebe29540288cb68bfe79504
Goran Miskovic <schkovich@gmail.com>
2015-01-04 19:04:24 +0100

    Cannot interpolate variable in node definition. Reverting.

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,7 +2,7 @@ Exec {
   path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/local/bin']
 }
 
-node "${fqdn}" {
+node "local.wordpress.dev" {
 
   stage { 'preinstall':
     before => Stage['main']
